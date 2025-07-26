55b9ed8b15af4448ee28c5dad3943940440a637f
Nitin Kumar Shah <shahnitin500@gmail.com>
2025-07-26 10:28:19 +0545

    funcanality

---
+++ function.js
@@ -0,0 +1,23 @@
+// function 
+// piece of code that handles some functionality
+
+function greet(name) {
+    console.log ("Hello how are you " + name);
+
+}
+greet("Nitin");
+greet("Baba");
+
+
+function square(num) {
+    console.log(num * num);
+}
+square(2);
+square(5);
+
+function addition( num1, num2) {
+    console.log(num1 + num2);
+
+}
+addition (2, 10);
+addition (5, 10);
