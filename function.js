ff683b22219bcb20919f9f9495613cbc5f2599b6
Nitin Kumar Shah <shahnitin500@gmail.com>
2025-07-26 22:00:39 +0545

    add Function Return Type

--- function.js
+++ function.js
@@ -1,23 +1,31 @@
-// function 
-// piece of code that handles some functionality
+// // function
+// // piece of code that handles some functionality
 
-function greet(name) {
-    console.log ("Hello how are you " + name);
+// function greet(name) {
+//   console.log("Hello how are you " + name);
+// }
+// greet("Nitin");
+// greet("Baba");
 
+// function square(num) {
+//   console.log(num * num);
+// }
+// square(2);
+// square(5);
+
+// function addition(num1, num2) {
+//   console.log(num1 + num2);
+// }
+// addition(2, 10);
+// addition(5, 10);
+   
+
+
+// Return Type 
+function percentage(score, total) {
+    const percent = (score / total) * 100;
+    return percent;
 }
-greet("Nitin");
-greet("Baba");
 
-
-function square(num) {
-    console.log(num * num);
-}
-square(2);
-square(5);
-
-function addition( num1, num2) {
-    console.log(num1 + num2);
-
-}
-addition (2, 10);
-addition (5, 10);
+const result = percentage(100, 150);
+console.log(result);
