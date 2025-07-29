d8ecdbaa7276d923a8c9fd26354156e649d66214
Nitin Kumar Shah <shahnitin500@gmail.com>
2025-07-29 21:15:08 +0545

    added

--- modules/https.js
+++ modules/https.js
@@ -1,13 +1,24 @@
 import http from "http";
 
 const server = http.createServer((request, response) => {
-    response.end("hello, I am Nitin.");
+console.log(request.method);
+// console.log(`Received ${request.method} request for ${request.url}`);
+
+    const data = {
+    name: "Nitin",
+    age: 25,
+    city: "Jhapa",
+};
+
+response.writeHead(200, { "content-type": "application/json" });
+response.end(JSON.stringify(data));
+
+// console.log("<h1> Server is running on port 5000 </h1>");
 });
+
+
 
 server.listen(5000, () => {
     console.log("Server is running on port 5000");
 });
 
-
-  
-// console.log("hello")
