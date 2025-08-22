0c3129b87c9c08f94f3c643c914d6e50467636c3
Nitin Kumar Shah <shahnitin500@gmail.com>
2025-08-22 19:06:55 +0545

    fix: update orderRoute.js to correct import paths and enhance role-based authentication

--- src/routes/orderRoute.js
+++ src/routes/orderRoute.js
@@ -1,12 +1,33 @@
 import express from "express";
-import orderController from "../controller/orderController.js";
-import auth from "../middlewares/auth.js";
+import orderController from "../controllers/orderController.js";
+import roleBasedAuth from "../middlewares/roleBasedAuth.js";
+import { ADMIN, MERCHANT } from "../constants/roles.js";
 
 const router = express.Router();
 
-//URL: /api/orders
-router.get("/", auth,  orderController.getOrders);
-router.post("/", auth, orderController.createOrders);
-router.delete("/:id", auth, orderController.deleteOrderOrders);
+// URL: /api/orders
+router.get("/", roleBasedAuth(ADMIN), orderController.getOrders);
+
+// URL: /api/orders/user
+router.get("/user", orderController.getOrdersByUser);
+
+router.get(
+  "/merchant",
+  roleBasedAuth(MERCHANT),
+  orderController.getOrdersOfMerchant
+);
+
+router.get("/:id", roleBasedAuth(ADMIN), orderController.getOrderById);
+
+router.post("/", orderController.createOrder);
+
+router.put("/:id", orderController.updateOrder);
+
+router.delete("/:id", orderController.deleteOrder);
+
+// URL: /api/orders/:id/payment/khalti
+router.post("/:id/payment/khalti", orderController.orderPaymentViaKhalti);
+
+router.put("/:id/confirm-payment", orderController.confirmOrderPayment);
 
 export default router;
